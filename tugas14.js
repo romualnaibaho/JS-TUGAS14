3316f6a6c6a8e9d65f01297d1518a00f8688ccb7
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:50:17 +0700

    js

---
+++ tugas14.js
@@ -0,0 +1,13 @@
+
+var umur = 20;
+
+var result = 37 / umur;
+
+var round = Math.round(result);
+var ceil = Math.ceil(result);
+var floor = Math.floor(result);
+
+console.log('Tanpa Fungsi Math :', result);
+console.log('Dengan Fungsi Math.Ceil :', ceil);
+console.log('Dengan Fungsi Math.Round :', round);
+console.log('Dengan Fungsi Math.Floor :', floor);
